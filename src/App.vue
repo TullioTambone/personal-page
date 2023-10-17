b128d509097db09ce518f06e0490818cfe58dccd
TullioTambone <tulliotambone97@gmail.com>
2023-10-17 13:00:25 +0200

    palette & nav  & fine cursore

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,54 @@
 export default{
   data(){
     return{}
+  },
+  mounted(){
+    this.cursorFollow();
+  },
+  methods:{
+    cursorFollow(){
+      window.addEventListener("mousemove", this.cursor);
+    },
+
+    cursor(e){
+      //pageX and Y 
+      let mouse = document.querySelector("#cursor");
+      mouse.style.transform = `translate(${e.pageX - 12}px, ${e.pageY - 14}px)`;
+
+      let inner = document.querySelector("#inner-cursor");
+      inner.style.transform = `translate(${e.pageX - 3}px, ${e.pageY - 5}px)`;
+    },
   }
 }
 </script>
 
 <template>
-  <router-view>
-  </router-view>
+  <div id="cursor"></div>
+  <div id="inner-cursor"></div>
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
-@use './style/main.scss';
+@import './style/main.scss';
+
+#cursor{
+  background-color: transparent;
+  border: 1px solid rgba($color: $atomic-tangerine, $alpha: 0.4);
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  position: fixed;
+  pointer-events: none;
+  transition: transform 0.6s ease;
+}
+
+#inner-cursor{
+  background-color: rgba($color: $atomic-tangerine, $alpha: 0.4);
+  border-radius: 50%;
+  width: 7px;
+  height: 7px;
+  position: fixed;
+  pointer-events: none;
+  transition: transform 0.3s ease;
+}
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,13 +1,56 @@
 <script>
-export default{
+import navbar from '../sections/navbar.vue';
 
+export default{
+    data(){
+        return{}
+    },
+    components:{
+        navbar
+    },
+    mounted(){
+        this.overMouse();
+    },
+    methods:{
+        overMouse(){
+            let navLinks = document.querySelectorAll(".nav-links");
+            let cursor = document.querySelector("#cursor");
+            let inner = document.querySelector("#inner-cursor");
+
+            navLinks.forEach(link => {
+                link.addEventListener("mouseover", () => {
+                    cursor.style.width = '33px';
+                    cursor.style.height = '33px';
+
+                    inner.style.width = '15px';
+                    inner.style.height = '15px';
+                });
+                
+                link.addEventListener("mouseout", () => {
+                    cursor.style.width = '25px';
+                    cursor.style.height = '25px';
+
+                    inner.style.width = '7px';
+                    inner.style.height = '7px';
+                });
+            });
+        }
+    }
 }
 </script>
 
 <template>
-    <h1>hello world</h1>
+    <navbar></navbar>
+    <div class="content">
+        <a href="" class="nav-links">bellaaaaaa</a>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+@use '../style/main.scss';
+.content{
+    height: 100vh;
+    width: 100%;
+    border: 1px solid red;
+}
 </style>
